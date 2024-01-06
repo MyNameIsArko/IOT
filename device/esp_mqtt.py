3269bb6cd3d1c6c3f23de31642c7bdf0437c35fc
Adrian Jaśkowiec <ajaskowiec@onet.eu>
2024-01-06 16:24:47 +0100

    asd

--- device/esp_mqtt.py
+++ device/esp_mqtt.py
@@ -58,8 +58,8 @@ class Broker:
                     humidity_txt = str(json_measurements["humidity"])
                     humidity_msg = self.encryption.encrypt(humidity_txt)
 
-                    temperature_encoded = ubinascii.b2a_base64(temperature_msg)
-                    humidity_encoded = ubinascii.b2a_base64(humidity_msg)
+                    temperature_encoded = ubinascii.a2b_base64(temperature_msg)
+                    humidity_encoded = ubinascii.a2b_base64(humidity_msg)
 
                     self.client.publish(self.temperature_topic, temperature_encoded)
                     self.client.publish(self.humidity_topic, humidity_encoded)
