aaaf0639ceaf11ea47cbedb93f9638561975f85d
Adrian Jaśkowiec <ajaskowiec@onet.eu>
2024-01-06 15:47:52 +0100

    crypto number to string

--- device/esp_crypto.py
+++ device/esp_crypto.py
@@ -2,10 +2,15 @@ import uos
 from ucryptolib import aes
 import ulogging
 
-log = ulogging.getLogger('CRYPTO')
+log = ulogging.getLogger("CRYPTO")
 
-class Encryption :
+
+class Encryption:
     def __init__(self, key, iv):
+        if isinstance(key, int):
+            key = str(key)
+        if isinstance(iv, int):
+            iv = str(iv)
         if isinstance(key, str):
             key = key.encode()
         if isinstance(iv, str):
@@ -20,18 +25,17 @@ class Encryption :
     def unpad(self, txt):
         log.info("Unpadding text")
         return txt.strip()
-    
+
     def encrypt(self, txt):
         padded = self.pad(txt)
         log.info("Creating cipher")
         cipher = aes(self.key, 2, self.iv)
         log.info("Encrypting data")
         return cipher.encrypt(padded)
-    
+
     def decrypt(self, data):
         log.info("Creating cipher")
         decipher = aes(self.key, 2, self.iv)
         log.info("Decrypting data")
         padded_txt = decipher.decrypt(data)
         return self.unpad(padded_txt)
-
